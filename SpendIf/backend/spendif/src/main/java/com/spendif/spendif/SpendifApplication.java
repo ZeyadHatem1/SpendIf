75bbd81e061848a2a7d0e4514d08f60a5c0de2f6
ZeyadHatem1 <Zeyadbagoury@gmail.com>
2025-08-17 10:57:40 +0400

    Spring

---
+++ SpendIf/backend/spendif/src/main/java/com/spendif/spendif/SpendifApplication.java
@@ -0,0 +1,13 @@
+package com.spendif.spendif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendifApplication.class, args);
+	}
+
+}
---
+++ SpendIf/backend/spendif/src/test/java/com/spendif/spendif/SpendifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendif.spendif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
