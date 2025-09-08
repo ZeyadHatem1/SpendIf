ee59c5d0d52bafc9f9daa04dbc3109b87b6f7399
ZeyadHatem1 <Zeyadbagoury@gmail.com>
2025-09-09 02:34:05 +0400

    in process debugging

--- SpendIf/backend/spendif/src/main/java/com/spendif/spendif/SpendifApplication.java
+++ SpendIf/backend/spendif/src/main/java/com/spendif/spendif/SpendifApplication.java
@@ -2,12 +2,14 @@ package com.spendif.spendif;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.spendif.spendif")
+@EntityScan(basePackages = "com.spendif.spendif.models")       
+@EnableJpaRepositories(basePackages = "com.spendif.spendif.repositories") 
 public class SpendifApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpendifApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpendifApplication.class, args);
+    }
 }
